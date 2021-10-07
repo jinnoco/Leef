dec759a4dac4cf9f25bcefabc986eb79ae848066
nofprog <k.3217@gmail.com>
2021-10-07 11:47:27 +0900

    ディレクトリ修正

---
+++ Leef/Views/MainTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  Leef
+//
+//  Created by J on 2021/07/18.
+//
+
+import UIKit
+import NeumorphismTab
+
+
+
+class MainTabBarController: NeumorphismTabBarController {
+    
+
+    
+    var color = MainColor()
+    
+    override func setupView() {
+        
+        let timeline = NeumorphismTabBarItem(icon: UIImage(systemName: "newspaper")!, title: "")
+        let myPage = NeumorphismTabBarItem(icon: UIImage(systemName: "person.crop.circle")!, title: "")
+        let supportPage = NeumorphismTabBarItem(icon: UIImage(systemName: "info.circle")!, title: "")
+        
+        view.backgroundColor = color.backColor
+        
+        let timelineViewController = TimelineViewController()
+        let timelineNVC = UINavigationController(rootViewController: timelineViewController)
+        
+        let myPageViewController = MyPageViewController()
+        let myPageNVC = UINavigationController(rootViewController: myPageViewController)
+        
+        let supportPageViewController = SupportPageViewController()
+        let supportPageNVC = UINavigationController(rootViewController: supportPageViewController)
+        
+        setTabBar(items: [timeline, myPage, supportPage])
+        setViewControllers([timelineNVC, myPageNVC, supportPageNVC], animated: false)
+        
+    }
+}
