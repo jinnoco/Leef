9da287a30a1b424fcaf91471c7425854d0f7c9d2
nofprog <k.3217@gmail.com>
2021-10-09 01:22:30 +0900

    スペース修正

--- Leef/Views/Tutorials/MyPageTutorialViewController.swift
+++ Leef/Views/Tutorials/MyPageTutorialViewController.swift
@@ -50,7 +50,7 @@ class MyPageTutorialViewController: UIViewController {
     
     func setTutorialImageView() {
         tutorialImageView.translatesAutoresizingMaskIntoConstraints                                         = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive             = true
+        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive              = true
         tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive      = true
         tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive   = true
         tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive       = true
--- Leef/Views/Tutorials/TimelineTutorialViewController.swift
+++ Leef/Views/Tutorials/TimelineTutorialViewController.swift
@@ -51,7 +51,7 @@ class TimelineTutorialViewController: UIViewController {
     
     func setTutorialImageView() {
         tutorialImageView.translatesAutoresizingMaskIntoConstraints                                         = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive             = true
+        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive              = true
         tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive      = true
         tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive   = true
         tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive       = true
