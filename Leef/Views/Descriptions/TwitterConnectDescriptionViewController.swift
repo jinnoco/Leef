9464f22a2a027293809dbdf9d27ea4a5d84ddfd0
nofprog <k.3217@gmail.com>
2021-10-08 10:52:41 +0900

    画像のセットをimage literalに変更

--- Leef/Views/Descriptions/TwitterConnectDescriptionViewController.swift
+++ Leef/Views/Descriptions/TwitterConnectDescriptionViewController.swift
@@ -23,16 +23,16 @@ class TwitterConnectDescriptionViewController: UIPageViewController {
     func configureTutorialImageView() {
         view.addSubview(tutorialImageView)
         tutorialImageView.contentMode = .scaleAspectFit
-        tutorialImageView.image = UIImage(named: "TwitterConnect_Description")
+        tutorialImageView.image = #imageLiteral(resourceName: "TwitterConnect_Description")
         setTutorialImageView()
     }
     
     func setTutorialImageView() {
-        tutorialImageView.translatesAutoresizingMaskIntoConstraints = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 10).isActive = true
-        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
-        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        tutorialImageView.translatesAutoresizingMaskIntoConstraints                                         = false
+        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 10).isActive             = true
+        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive      = true
+        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive   = true
+        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive       = true
     }
 
 }
