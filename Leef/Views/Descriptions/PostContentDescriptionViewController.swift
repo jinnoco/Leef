823a754ffbfd481ee6672b7870e9b06f0ad1d9ce
nofprog <k.3217@gmail.com>
2021-10-09 01:21:31 +0900

    スペース修正

--- Leef/Views/Descriptions/PostContentDescriptionViewController.swift
+++ Leef/Views/Descriptions/PostContentDescriptionViewController.swift
@@ -30,10 +30,10 @@ class PostContentDescriptionViewController: UIPageViewController {
     
     func setTutorialImageView() {
         tutorialImageView.translatesAutoresizingMaskIntoConstraints = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 10).isActive             = true
-        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive      = true
-        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive   = true
-        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive       = true
+        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive             = true
+        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive     = true
+        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive  = true
+        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive      = true
     }
 
 }
