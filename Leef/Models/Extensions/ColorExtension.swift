15c2d184d2512b57760cab62d0cf700d5a2e3931
nofprog <k.3217@gmail.com>
2021-10-08 09:07:44 +0900

    コメント修正

--- Leef/Models/Extensions/ColorExtension.swift
+++ Leef/Models/Extensions/ColorExtension.swift
@@ -10,29 +10,30 @@ import UIKit
 
 struct MainColor {
     
-    //#FFFFFF
+    // #FFFFFF
     var whiteColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     
-    //#1EA0F1
+    // #1EA0F1
     var blueColor = #colorLiteral(red: 0.1176470588, green: 0.6274509804, blue: 0.9450980392, alpha: 1)
     
-    //#FAFAFA
+    // #FAFAFA
     var lightGrayColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
     
-    //#727272
+    // #727272
     var darkGrayColor = #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1)
     
-    //#F8F8F8  main background
+    // #F8F8F8  main background
     var backColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
     
-    //#D9D9D9
+    // #D9D9D9
     var darkShadow = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
     
-    //#FFFFFF
+    // #FFFFFF
     var lightShadow = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     
-    //#EDEDED
+    // #EDEDED
     var cellColor = #colorLiteral(red: 0.9294117647, green: 0.9294117647, blue: 0.9294117647, alpha: 1)
     
-    var greenColor = #colorLiteral(red: 0.2941176471, green: 0.6470588235, blue: 0.03137254902, alpha: 1)
+    // #4B9908
+    var greenColor = #colorLiteral(red: 0.2941176471, green: 0.6, blue: 0.03137254902, alpha: 1)
 }
