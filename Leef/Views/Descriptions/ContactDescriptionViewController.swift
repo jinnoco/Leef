1a2854cc83fe9483b5723f05556138219fa90a33
nofprog <k.3217@gmail.com>
2021-10-08 10:57:03 +0900

    画像のセットをimage literalに変更

--- Leef/Views/Descriptions/ContactDescriptionViewController.swift
+++ Leef/Views/Descriptions/ContactDescriptionViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ContactDescriptionViewController: UIPageViewController {
-
     
     let tutorialImageView = UIImageView()
     var color = MainColor()
@@ -23,16 +22,16 @@ class ContactDescriptionViewController: UIPageViewController {
     func configureTutorialImageView() {
         view.addSubview(tutorialImageView)
         tutorialImageView.contentMode = .scaleAspectFit
-        tutorialImageView.image = UIImage(named: "Contact_Description")
+        tutorialImageView.image = #imageLiteral(resourceName: "Contact_Description")
         setTutorialImageView()
     }
     
     func setTutorialImageView() {
-        tutorialImageView.translatesAutoresizingMaskIntoConstraints = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 10).isActive = true
-        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
-        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        tutorialImageView.translatesAutoresizingMaskIntoConstraints                                         = false
+        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 10).isActive             = true
+        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive      = true
+        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive   = true
+        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive       = true
     }
 
 }
