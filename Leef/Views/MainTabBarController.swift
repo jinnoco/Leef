c2e0bd88a74c9b77f730502b3f25c4bfd58afef3
nofprog <k.3217@gmail.com>
2021-10-08 10:19:11 +0900

    guard文で強制アンラップを修正

--- Leef/Views/MainTabBarController.swift
+++ Leef/Views/MainTabBarController.swift
@@ -12,15 +12,17 @@ import NeumorphismTab
 
 class MainTabBarController: NeumorphismTabBarController {
     
-
-    
     var color = MainColor()
     
     override func setupView() {
         
-        let timeline = NeumorphismTabBarItem(icon: UIImage(systemName: "newspaper")!, title: "")
-        let myPage = NeumorphismTabBarItem(icon: UIImage(systemName: "person.crop.circle")!, title: "")
-        let supportPage = NeumorphismTabBarItem(icon: UIImage(systemName: "info.circle")!, title: "")
+        guard let newspaperIcon = UIImage(systemName: "newspaper") else { return }
+        guard let personIcon = UIImage(systemName: "person.crop.circle") else { return }
+        guard let infoIcon = UIImage(systemName: "info.circle") else { return }
+        
+        let timeline = NeumorphismTabBarItem(icon: newspaperIcon, title: "")
+        let myPage = NeumorphismTabBarItem(icon: personIcon, title: "")
+        let supportPage = NeumorphismTabBarItem(icon: infoIcon, title: "")
         
         view.backgroundColor = color.backColor
         
