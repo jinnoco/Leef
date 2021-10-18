4a5fff90f0dbb01c2fed69653652057de08fc96c
nofprog <k.3217@gmail.com>
2021-10-18 18:45:19 +0900

    アクセス修飾子修正、メソッド切り出し

--- Leef/Controllers/Timeline/TimelineViewController.swift
+++ Leef/Controllers/Timeline/TimelineViewController.swift
@@ -12,31 +12,27 @@ import SoftUIView
 
 class TimelineViewController: UIViewController, LoadDelegate {
     
-    let postPageViewContorller = PostPageViewController()
-    let checkPermission = CheckPermission()
-    let color = MainColor()
-    let loadDBModel = LoadDBModel()
-    
     // UI
-    let tableView = UITableView()
-    let timelineCellId = "timelineCell"
-    let titleImage = UIImageView()
+    private var tableView = UITableView()
+    private var titleImage = UIImageView()
     
+    private let timelineCellId = "timelineCell"
+    
+    public var postPageViewContorller = PostPageViewController()
+    public var checkPermission = CheckPermission()
+    private var color = MainColor()
+    private var loadDBModel = LoadDBModel()
     
     
     override func loadView() {
         super.loadView()
-        
         configureNav()
         configureTableView()
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        print(Auth.auth().currentUser?.displayName ?? "Twitter??")
         // 全体のPostDataをロード
         loadDBModel.loadPostData()
         tableView.reloadData()
@@ -83,7 +79,7 @@ class TimelineViewController: UIViewController, LoadDelegate {
     
     
     // NavigationBar
-    func configureNav() {
+    private func configureNav() {
         // navigationBarを表示
         navigationController?.setNavigationBarHidden(false, animated: true)
         // rightBarButtonItem(投稿作成)
@@ -112,14 +108,14 @@ class TimelineViewController: UIViewController, LoadDelegate {
     
     
     @objc
-    func toNewPostPage() {
+    private func toNewPostPage() {
         // 新規投稿作成ページに遷移
         let postPageViewController = PostPageViewController()
         postPageViewController.modalPresentationStyle = .fullScreen
         present(postPageViewController, animated: true, completion: nil)
     }
     
-    func configureTableView() {
+    private func configureTableView() {
         view.addSubview(tableView)
         tableView.rowHeight = UITableView.automaticDimension
         tableView.register(TimelineCell.self, forCellReuseIdentifier: timelineCellId)
@@ -129,19 +125,18 @@ class TimelineViewController: UIViewController, LoadDelegate {
         
     }
     
-    func setTableView() {
+    private func setTableView() {
         configureRefreshControl()
         tableView.pin(to: view)
     }
     
-    func configureRefreshControl() {
+    private func configureRefreshControl() {
         tableView.refreshControl = UIRefreshControl()
         tableView.refreshControl?.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
     }
     
     @objc
-    func handleRefreshControl() {
-        
+    private func handleRefreshControl() {
         DispatchQueue.main.async {
             self.tableView.reloadData()
             self.tableView.refreshControl?.endRefreshing()
@@ -161,9 +156,7 @@ extension TimelineViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         if let cell = tableView.dequeueReusableCell(withIdentifier: timelineCellId, for: indexPath) as? TimelineCell {
-            
             // loadDBModel.dataSetsからそれぞれ情報を取得
             let timelineImage = cell.timelineImageView
             timelineImage.loadImage(with: loadDBModel.dataSets[indexPath.row].postImageURLString)
@@ -178,18 +171,14 @@ extension TimelineViewController: UITableViewDelegate, UITableViewDataSource {
             cell.selectionStyle = .none
             
             return cell
-            
         } else {
-            
             return UITableViewCell()
         }
-        
-        
         
     }
     
     
-    func dateFormatForDateLabel(postDate: Timestamp) -> String {
+    private func dateFormatForDateLabel(postDate: Timestamp) -> String {
         // 投稿された日付をTimestampからラベル表示用に変換
         let postDateValue = postDate.dateValue()
         let  formatter = DateFormatter()
@@ -200,8 +189,6 @@ extension TimelineViewController: UITableViewDelegate, UITableViewDataSource {
         return dateString
     }
     
-    
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // 値を渡して画面遷移
@@ -211,102 +198,9 @@ extension TimelineViewController: UITableViewDelegate, UITableViewDataSource {
         selectViewController.profileImageString = loadDBModel.dataSets[indexPath.row].profileImageURLString
         selectViewController.textView.text = loadDBModel.dataSets[indexPath.row].comment
         selectViewController.userId = loadDBModel.dataSets[indexPath.row].userId
-        
         selectViewController.doc = loadDBModel.dataSets[indexPath.row].docId
         
         navigationController?.pushViewController(selectViewController, animated: true)
-        
-    }
-    
-    
-    
-}
-
-extension TimelineViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    func openCamera() {
-        let sourceType: UIImagePickerController.SourceType = .camera
-        // カメラ利用チェック
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            
-            let cameraPicker = UIImagePickerController()
-            cameraPicker.allowsEditing = true
-            cameraPicker.sourceType = sourceType
-            cameraPicker.delegate = self
-            self.present(cameraPicker, animated: true, completion: nil)
-        }
-    }
-    
-    
-    func openLibrary() {
-        let sourceType: UIImagePickerController.SourceType = .photoLibrary
-        // フォトライブラリ利用チェック
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            
-            let cameraPicker = UIImagePickerController()
-            cameraPicker.allowsEditing = true
-            cameraPicker.sourceType = sourceType
-            cameraPicker.delegate = self
-            self.present(cameraPicker, animated: true, completion: nil)
-        }
-        
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        
-        if info[.originalImage] as? UIImage != nil {
-            // 画像をセットして画面遷移
-            if let selectedImage = info[.originalImage] as? UIImage {
-                postPageViewContorller.postImage = selectedImage
-                picker.dismiss(animated: true, completion: nil)
-                postPageViewContorller.modalPresentationStyle = .fullScreen
-                present(postPageViewContorller, animated: true, completion: nil)
-                
-            } else {
-                return
-            }
-            
-            
-        }
-    }
-    
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    @objc
-    func showAlert() {
-        
-        let user = Auth.auth().currentUser?.displayName
-        
-        if user != nil {
-            
-            let alertController = UIAlertController(title: "新しい投稿を作成します", message: "どちらを使用しますか?", preferredStyle: .actionSheet)
-            
-            alertController.addAction(UIAlertAction(title: "カメラで撮影", style: .default, handler: { _ in
-                self.openCamera()
-            }))
-            alertController.addAction(UIAlertAction(title: "ライブラリから選択", style: .default, handler: { _ in
-                self.openLibrary()
-            }))
-            alertController.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
-            
-            self.present(alertController, animated: true, completion: nil)
-            
-        } else if user == nil {
-            
-            let alertController = UIAlertController(title: "確認", message: "投稿機能を利用するにはTwitterアカウントを連携してください", preferredStyle: .alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            
-            present(alertController, animated: true, completion: nil)
-            
-            
-        }
-        
-        
         
     }
     
