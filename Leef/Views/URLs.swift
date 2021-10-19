51d78933d697615f8b342f09c7cdc7388449bff4
nofprog <k.3217@gmail.com>
2021-10-19 13:18:21 +0900

    URLを定数として定義

---
+++ Leef/Views/URLs.swift
@@ -0,0 +1,25 @@
+//
+//  URLs.swift
+//  Leef
+//
+//  Created by J on 2021/10/18.
+//
+
+import Foundation
+
+struct URLs {
+    // 利用規約ページ
+    let tosPageURL = "https://site-2671642-9203-8355.mystrikingly.com"
+    
+    // プライバシーポリシー
+    let privacyPolicyPageURL = "https://site-2671642-386-3077.mystrikingly.com"
+    
+    // ユーザー報告
+    let reportPageURL = "https://site-2671642-9832-2847.mystrikingly.com"
+    
+    //LeefTwitterアカウント
+    let contactPageURL = "https://twitter.com/LeefApp_"
+    
+    //Twitter
+    let twitterURL = "https://twitter.com/?lang=ja"
+}
