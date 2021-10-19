66134b73e17590b807fcce971e25208bd57b974e
nofprog <k.3217@gmail.com>
2021-10-19 18:08:00 +0900

    UIColorを継承したクラスとして定義

--- Leef/Models/Extensions/ColorExtension.swift
+++ Leef/Models/Extensions/ColorExtension.swift
@@ -8,32 +8,51 @@
 
 import UIKit
 
-struct MainColor {
+class MainColor: UIColor {
     
     // #FFFFFF
-    var whiteColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var whiteColor: UIColor {
+        return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
     
     // #1EA0F1
-    var blueColor = #colorLiteral(red: 0.1176470588, green: 0.6274509804, blue: 0.9450980392, alpha: 1)
+    var blueColor: UIColor {
+        return #colorLiteral(red: 0.1176470588, green: 0.6274509804, blue: 0.9450980392, alpha: 1)
+    }
     
     // #FAFAFA
-    var lightGrayColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+    var lightGrayColor: UIColor {
+        return #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+    }
     
     // #727272
-    var darkGrayColor = #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1)
+    var darkGrayColor: UIColor {
+        return #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1)
+    }
     
     // #F8F8F8  main background
-    var backColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+    var backColor: UIColor {
+        return #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+    }
     
     // #D9D9D9
-    var darkShadow = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+    var darkShadow: UIColor {
+        return #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+    }
     
     // #FFFFFF
-    var lightShadow = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var lightShadow: UIColor {
+        return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
     
     // #EDEDED
-    var cellColor = #colorLiteral(red: 0.9294117647, green: 0.9294117647, blue: 0.9294117647, alpha: 1)
+    var cellColor: UIColor {
+        return #colorLiteral(red: 0.9294117647, green: 0.9294117647, blue: 0.9294117647, alpha: 1)
+    }
     
-    // #4B9908
-    var greenColor = #colorLiteral(red: 0.2941176471, green: 0.6, blue: 0.03137254902, alpha: 1)
+    // #88A32A
+    var greenColor: UIColor {
+        return #colorLiteral(red: 0.5333333333, green: 0.6392156863, blue: 0.1647058824, alpha: 1)
+    }
+    
 }
