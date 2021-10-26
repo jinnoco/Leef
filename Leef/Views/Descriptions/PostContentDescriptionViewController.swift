8d43d08457492b8f5ce4668034671e7bf234acb5
nofprog <k.3217@gmail.com>
2021-10-26 11:04:40 +0900

    setDiscriptionメソッド

--- Leef/Views/Descriptions/PostContentDescriptionViewController.swift
+++ Leef/Views/Descriptions/PostContentDescriptionViewController.swift
@@ -9,30 +9,11 @@ import UIKit
 
 class PostContentDescriptionViewController: UIPageViewController {
     
-    private var tutorialImageView = UIImageView()
-    private var color = MainColor()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = color.backColor
-        configureTutorialImageView()
-        
-    }
-    
-    private func configureTutorialImageView() {
-        view.addSubview(tutorialImageView)
-        tutorialImageView.contentMode = .scaleAspectFit
-        tutorialImageView.image = #imageLiteral(resourceName: "Post_Description")
-        setTutorialImageView()
-    }
-    
-    private func setTutorialImageView() {
-        tutorialImageView.translatesAutoresizingMaskIntoConstraints                                        = false
-        tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive             = true
-        tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive     = true
-        tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive  = true
-        tutorialImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive      = true
+        SetDiscription().setDiscription(view: view, discriptionImage: #imageLiteral(resourceName: "Post_Description"))
+
     }
     
 }
