134970f67b6fb41dd7c4315dc43d53a97cd4438e
nofprog <k.3217@gmail.com>
2021-10-31 14:10:05 +0900

    各URLを構造体で管理

--- Leef/Views/URLs.swift
+++ Leef/Views/URLs.swift
@@ -9,17 +9,27 @@ import Foundation
 
 struct URLs {
     // 利用規約ページ
-    let tosPageURL = "https://site-2671642-9203-8355.mystrikingly.com"
+    var tosPageURL: String {
+        return "https://site-2671642-9203-8355.mystrikingly.com"
+    }
     
     // プライバシーポリシー
-    let privacyPolicyPageURL = "https://site-2671642-386-3077.mystrikingly.com"
+    var privacyPolicyPageURL: String {
+        return "https://site-2671642-386-3077.mystrikingly.com"
+    }
     
     // ユーザー報告
-    let reportPageURL = "https://site-2671642-9832-2847.mystrikingly.com"
+    var reportPageURL: String {
+        return "https://site-2671642-9832-2847.mystrikingly.com"
+    }
     
     // LeefTwitterアカウント
-    let contactPageURL = "https://twitter.com/LeefApp_"
+    var contactPageURL: String {
+        return "https://twitter.com/LeefApp_"
+    }
     
     // Twitter
-    let twitterURL = "https://twitter.com/?lang=ja"
+    var twitterURL: String {
+        return "https://twitter.com/?lang=ja"
+    }
 }
