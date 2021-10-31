7c5f3b68ef930d492e91cc2651c2e09938f16844
nofprog <k.3217@gmail.com>
2021-10-31 15:13:25 +0900

    修正前に変更

--- Leef/Models/DB/LoadDBModel.swift
+++ Leef/Models/DB/LoadDBModel.swift
@@ -16,12 +16,14 @@ class LoadDBModel {
     
     public var dataSets = [DataSet]()
     public var myDataSet = [MyDataSet]()
-    public var database = Firestore.firestore()
+    
+    var db = Firestore.firestore()
+    
     public var myUid = Auth.auth().currentUser?.uid
     public weak var loadDelegate: LoadDelegate?
     
-    public func loadPostData() {
-        database.collection("post").order(by: "postDate").addSnapshotListener { [self] snapshot, error in
+    func loadPostData() {
+        db.collection("post").order(by: "postDate").addSnapshotListener { [self] snapshot, error in
             
             self.dataSets = []
             
@@ -30,47 +32,39 @@ class LoadDBModel {
                 return
             }
             
-            guard let snapshotDoc = snapshot?.documents else { return }
-            
-            for doc in snapshotDoc {
+            if let snapshotDoc = snapshot?.documents {
                 
-                let data = doc.data()
-                
-                guard let uid = data["uid"] as? String else { return }
-                guard let username = data["username"] as? String else { return }
-                guard let postImageData = data["postImageURLString"] as? String else { return }
-                guard let comment = data["comment"] as? String else { return }
-                guard let profileImageURLString = data["profileImageURLString"] as? String else { return }
-                guard let userId = data["userId"] as? String else { return }
-                guard let docId = doc.documentID as? String else { return }
-                guard let postDate = data["postDate"] as? Timestamp else { return }
-                
-                
-                let newDataSet = DataSet(uid: uid,
-                                         username: username,
-                                         postImageURLString: postImageData,
-                                         comment: comment,
-                                         profileImageURLString: profileImageURLString,
-                                         userId: userId,
-                                         postDate: postDate,
-                                         docId: docId)
-                
-                self.dataSets.append(newDataSet)
-                self.dataSets.reverse()
-                self.loadDelegate?.doneLoad(check: 1)
-                
+                for doc in snapshotDoc {
+                    
+                    let data = doc.data()
+                    
+                    if let uid = data["uid"] as? String,
+                       let username = data["username"] as? String,
+                       let postImageData = data["postImageURLString"] as? String,
+                       let comment = data["comment"] as? String,
+                       let  profileImageURLString = data["profileImageURLString"]  as? String,
+                       let userId = data["userId"] as? String,
+                       let docId = doc.documentID as? String,
+                       let postDate = data["postDate"] as? Timestamp {
+                        
+                        
+                        let newDataSet = DataSet(uid: uid, username: username, postImageURLString: postImageData, comment: comment, profileImageURLString: profileImageURLString, userId: userId, postDate: postDate, docId: docId)
+                        
+                        self.dataSets.append(newDataSet)
+                        self.dataSets.reverse()
+                        self.loadDelegate?.doneLoad(check: 1)
+                        
+                    }
+                }
+                print("全体の投稿: \(dataSets.count)件")
             }
         }
-        print("全体の投稿: \(dataSets.count)件")
     }
-    
-    
-    
     
     
     func loadMyPostData() {
         
-        database.collection("post").order(by: "postDate").addSnapshotListener { [self]snapshot, error in
+        db.collection("post").order(by: "postDate").addSnapshotListener { [self]snapshot, error in
             
             self.myDataSet = []
             
@@ -79,40 +73,36 @@ class LoadDBModel {
                 return
             }
             
-            guard let snapshotDoc = snapshot?.documents else { return }
-            
-            for doc in snapshotDoc {
+            if let snapshotDoc = snapshot?.documents {
                 
-                let data = doc.data()
-                
-                guard let uid = data["uid"] as? String else { return }
-                guard let username = data["username"] as? String else { return }
-                guard let postImageData = data["postImageURLString"] as? String  else { return }
-                guard let comment = data["comment"] as? String else { return }
-                guard let  profileImageURLString = data["profileImageURLString"] as? String else { return }
-                guard let userId = data["userId"] as? String else { return }
-                guard let docId = doc.documentID as? String else { return }
-                guard let postDate = data["postDate"] as? Timestamp else { return }
-                
-                if myUid == uid {
+                for doc in snapshotDoc {
                     
-                    let newMyDataSet = MyDataSet(uid: uid,
-                                                 username: username,
-                                                 postImageData: postImageData,
-                                                 comment: comment,
-                                                 profileImageURLString: profileImageURLString,
-                                                 userId: userId,
-                                                 postDate: postDate,
-                                                 docId: docId)
+                    let data = doc.data()
                     
-                    self.myDataSet.append(newMyDataSet)
-                    self.myDataSet.reverse()
-                    
+                    if let uid = data["uid"] as? String,
+                       let username = data["username"] as? String,
+                       let postImageData = data["postImageURLString"] as? String,
+                       let comment = data["comment"] as? String,
+                       let  profileImageURLString = data["profileImageURLString"] as? String,
+                       let userId = data["userId"] as? String,
+                       let docId = doc.documentID as? String,
+                       let postDate = data["postDate"] as? Timestamp
+                    {
+                        
+                        if myUid == uid {
+                            let newMyDataSet = MyDataSet(uid: uid, username: username, postImageData: postImageData, comment: comment, profileImageURLString: profileImageURLString, userId: userId, postDate: postDate, docId: docId)
+                            
+                            self.myDataSet.append(newMyDataSet)
+                            self.myDataSet.reverse()
+                            
+                        }
+                    }
                 }
             }
+            print("myDataSetデータ受信完了")
+            print("自分の投稿: \(myDataSet.count)件")
+            self.loadDelegate?.doneLoad(check: 2)
         }
-        print("myDataSetデータ受信完了")
-        print("自分の投稿: \(myDataSet.count)件")
-        self.loadDelegate?.doneLoad(check: 2)
     }
+    
 }
