c6ae13fdae5819fb856443cca2440cf406ae9a6f
nofprog <k.3217@gmail.com>
2021-10-31 14:10:52 +0900

    ファイル名を定義

---
+++ Leef/Views/Animations/LottieFile.swift
@@ -0,0 +1,26 @@
+//
+//  LottieFile.swift
+//  Leef
+//
+//  Created by J on 2021/10/27.
+//
+
+import Lottie
+
+struct LottieAnimation {
+    
+    // firstView
+    var firstViewAnimation: String {
+        return "lf30_editor_mtdla6kp"
+    }
+    
+    // myPage
+    var myPageAnimation: String {
+        return "lf30_editor_zozlaqwf"
+    }
+    
+    // supportPage
+    var supportPageAnimation: String {
+        return "41384-customer-support-animation"
+    }
+}
